8234ab3326b4bd262b874c6fafb76a22b0205945
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-05-21 14:29:24 -0400

    blog grid and logo gallery added

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,30 @@
-function scaleHeight() {
-  const element = document.querySelector('.features-gallery__item');
+function scaleHeight(itemClass, factor) {
+  const element = document.querySelector('.' + itemClass);
 
   let width = window.getComputedStyle(element).getPropertyValue("width"),
-    height = (parseFloat(width, 10) * 2) + 'px';
+    height = (parseFloat(width, 10) * factor) + 'px';
   element.parentElement.style.setProperty("--item-height", height)
 }
-function scaleHeightM() {
-  const element = document.querySelector('.features-gallery__logo'),
-    elements = [...document.getElementsByClassName('features-gallery__logo')];
+function scaleHeightM(itemClass, factor = 1) {
+  const element = document.querySelector('.' + itemClass),
+    elements = [...document.getElementsByClassName(itemClass)];
 
   let width = window.getComputedStyle(element).getPropertyValue("width"),
-    height = parseFloat(width, 10) + 'px';
+    height = (parseFloat(width, 10) * factor) + 'px';
 
   elements.forEach(item => {
     item.style.setProperty("--item-height", height)
   })
 }
-scaleHeight()
-scaleHeightM()
+scaleHeight('features-gallery__item', 2)
+// scaleHeight('post', 1.2)
+scaleHeightM('features-gallery__logo')
+scaleHeightM('post__fig', .5)
 window.addEventListener('resize', e => {
-  scaleHeight()
-  scaleHeightM()
+  scaleHeight('features-gallery__item', 2)
+  // scaleHeight('post', 1.2)
+  scaleHeightM('features-gallery__logo')
+  // scaleHeightM('post__fig', .5)
+
 
 })
