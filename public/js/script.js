b1d89863a147a221b210581ac9846d8419bf4a3f
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-05-22 22:08:09 -0400

    resposive added

--- public/js/script.js
+++ public/js/script.js
@@ -17,14 +17,55 @@ function scaleHeightM(itemClass, factor = 1) {
   })
 }
 scaleHeight('features-gallery__item', 2)
+if (window.innerWidth < 513) {
+
+  scaleHeight('features-gallery__item', 1.4)
+}
 // scaleHeight('post', 1.2)
 scaleHeightM('features-gallery__logo')
-scaleHeightM('post__fig', .5)
+scaleHeightM('post__fig', .6)
 window.addEventListener('resize', e => {
   scaleHeight('features-gallery__item', 2)
+  if (window.innerWidth < 513) {
+
+    scaleHeight('features-gallery__item', 1.4)
+  }
   // scaleHeight('post', 1.2)
   scaleHeightM('features-gallery__logo')
-  // scaleHeightM('post__fig', .5)
+  scaleHeightM('post__fig', .6)
 
+  scaleBtnWidth('navTrigger', 1)
+  if (window.innerWidth < 513) {
 
+    scaleBtnWidth('chatTrigger', 1)
+  }
 })
+
+function scaleBtnWidth(itemId, factor) {
+  const item = document.getElementById(itemId),
+    height = window.getComputedStyle(item).getPropertyValue("height"),
+    width = (parseFloat(height, 10) * factor) + 'px';
+  // item.style.width = width;
+  console.log(height, width, item)
+  item.style.setProperty("--btn-width", width)
+
+}
+scaleBtnWidth('navTrigger', 1)
+if (window.innerWidth < 513) {
+
+  scaleBtnWidth('chatTrigger', 1)
+}
+
+function navBarMobileDisplay() {
+  const trigger = document.getElementById('navTrigger'),
+    nav = document.getElementById('mainNav'),
+    icon = document.getElementById('')
+
+  trigger.addEventListener('click', e => {
+    const icon = document.querySelector('.menu-icon')
+    console.log(e.target, icon)
+    icon.classList.toggle('menu-icon--active')
+    nav.classList.toggle('navbar--active')
+  })
+
+} navBarMobileDisplay()
